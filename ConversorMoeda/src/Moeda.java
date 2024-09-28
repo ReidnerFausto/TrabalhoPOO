c74fe2f47bbcf2f4b755ee64f3d32949d6d16d85
Reidner Marques <reidner.fausto123@gmail.com>
2024-09-28 19:11:24 -0300

    Add files via upload

---
+++ ConversorMoeda/src/Moeda.java
@@ -0,0 +1,17 @@
+class Moeda {
+    private String name;
+    private float taxa;
+
+    public Moeda(String name, float taxa) {
+        this.name = name;
+        this.taxa = taxa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double gettaxa() {
+        return taxa;
+    }
+}
